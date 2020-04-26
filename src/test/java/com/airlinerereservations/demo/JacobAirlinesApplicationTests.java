06f947b2a16b1a6a00f8df83ba0e900f0f39cc8a
Jacob Smith <jacob8smith@gmail.com>
2020-04-26 14:18:47 -0400

    Initial commit

---
+++ src/test/java/com/airlinerereservations/demo/JacobAirlinesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airlinerereservations.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JacobAirlinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
